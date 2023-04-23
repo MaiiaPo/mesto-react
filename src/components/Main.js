842ea5371a875463babe41dab4ec3978d0b6645d
Maiia <maiiapo.com@gmail.com>
2023-04-23 18:22:21 +0500

    Реализация открытия попапов.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,46 @@
 import avatar from "../images/avatar.jpg";
 function Main() {
+  function handleEditAvatarClick() {
+    const avatarEditButton = document.querySelector('.popup_type_edit-avatar')
+    avatarEditButton.classList.add('popup_opened');
+
+  }
+
+  function handleEditProfileClick() {
+    const profileEditButton = document.querySelector('.popup_type_edit')
+    profileEditButton.classList.add('popup_opened');
+
+  }
+
+  function handleAddPlaceClick() {
+    const addPlaceButton = document.querySelector('.popup_type_add')
+    addPlaceButton.classList.add('popup_opened');
+  }
+
   return (
     <main className="content">
       <section className="profile">
-        <div className="profile__avatar-block">
+        <div className="profile__avatar-block" onClick={handleEditAvatarClick}>
           <img className="profile__avatar" src={avatar} alt="Аватар"/>
         </div>
         <div className="profile__info">
           <div className="profile__title">
             <h1 className="profile__name">Жак-Ив Кусто</h1>
-            <button className="profile__edit link" type="button" aria-label="Редактировать"></button>
+            <button
+              className="profile__edit link"
+              type="button"
+              aria-label="Редактировать"
+              onClick={handleEditProfileClick}
+            />
           </div>
           <p className="profile__description">Исследователь океана</p>
         </div>
-        <button className="profile__add-button link" type="button" aria-label="Добавить"></button>
+        <button
+          className="profile__add-button link"
+          type="button"
+          aria-label="Добавить"
+          onClick={handleAddPlaceClick}
+        />
       </section>
       <section className="elements" aria-label="Места"></section>
     </main>
